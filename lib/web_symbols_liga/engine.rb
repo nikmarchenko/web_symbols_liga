9a2899fc0ba2310f655057a740a3983146869bfb
Nik Marchenko <nik.marchenko@gmail.com>
2013-09-07 15:31:33 +0400

    Fix: remove namespace isolation method

--- lib/web_symbols_liga/engine.rb
+++ lib/web_symbols_liga/engine.rb
@@ -1,7 +1,5 @@
 module WebSymbolsLiga
   class Engine < ::Rails::Engine
-    isolate_namespace WebSymbolsLiga
-
     config.generators do |g|                                                               
       g.test_framework   :rspec
       g.integration_tool :rspec
